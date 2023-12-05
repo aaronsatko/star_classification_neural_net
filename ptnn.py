5957a84911f7710ccb7a469ca24175fe185e5a10
aaronsatko <agsatko@gmail.com>
2023-12-04 21:47:46 -0500

    validation

--- ptnn.py
+++ ptnn.py
@@ -16,6 +16,9 @@ from sklearn.metrics import accuracy_score
 # calculate accuracy of a model
 import numpy as np
 
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(f"Using device: {device}")
+
 
 # load dataset
 data = pd.read_csv('star_classification_pruned.csv')
@@ -29,28 +32,38 @@ X = data[['u', 'g', 'r', 'i', 'z', 'redshift']].values
 # output data
 y = data['class'].values
 
-# training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+# training, testing, and validation sets
+# Split data into training (80%) and testing + validation (20%)
+X_train, X_temp, y_train, y_temp = train_test_split(X, y, test_size=0.2, random_state=42)
+# split data into validation (10%) and testing (10%)
+X_val, X_test, y_val, y_test = train_test_split(X_temp, y_temp, test_size=0.5, random_state=42)
+
 
 # standardization of features
-scalar = StandardScaler()
-X_train = scalar.fit_transform(X_train)
-X_test = scalar.transform(X_test)
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_val = scaler.transform(X_val)
+X_test = scaler.transform(X_test)
 
 # convert data into torch tensors
 
+# convert data into torch tensors
 X_train = torch.tensor(X_train, dtype=torch.float32)
+X_val = torch.tensor(X_val, dtype=torch.float32)
 X_test = torch.tensor(X_test, dtype=torch.float32)
 y_train = torch.tensor(y_train, dtype=torch.long)
+y_val = torch.tensor(y_val, dtype=torch.long)
 y_test = torch.tensor(y_test, dtype=torch.long)
 
 
 # Create tensor datasets
 train_dataset = TensorDataset(X_train, y_train)
+val_dataset = TensorDataset(X_val, y_val)
 test_dataset = TensorDataset(X_test, y_test)
 
 # batch handling
 train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True)
+val_loader = DataLoader(val_dataset, batch_size=64, shuffle=False)
 test_loader = DataLoader(test_dataset, batch_size=64, shuffle=False)
 
 # nn architecture
@@ -58,22 +71,24 @@ class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
         # First fully connected layer
-        self.fc1 = nn.Linear(6, 128)  # 6 input features
+        self.fc1 = nn.Linear(6, 128)
         # Second fully connected layer
+        #self.fc2 = nn.Linear(128, 128)
         self.fc2 = nn.Linear(128, 64)
         # Output layer, 3 classes
         self.fc3 = nn.Linear(64, 3)
 
     def forward(self, x):
-        # Apply ReLU activation function after first layer
+        # ReLU activation function after first layer
         x = F.relu(self.fc1(x))
-        # Apply ReLU activation function after second layer
+        # ReLU activation function after second layer
         x = F.relu(self.fc2(x))
-        # No activation needed in the output layer
+        #x = F.relu(self.fc3(x))
+        # No activation for out
         x = self.fc3(x)
         return x
     
-net = Net()
+net = Net().to(device)
 
 # loss function
 loss_func = nn.CrossEntropyLoss()
@@ -83,41 +98,46 @@ optimizer = optim.Adam(net.parameters(), lr=0.001)
 
 
 # train the neural net
-for epoch in range(20):  # loop over the dataset multiple times
+for epoch in range(20):
+    net.train()
     running_loss = 0.0
-    for i, data in enumerate(train_loader, 0):
-        # get the inputs; data is a list of [inputs, labels]
-        inputs, labels = data
-
-        # zero the parameter gradients
+    for inputs, labels in train_loader:
+        inputs, labels = inputs.to(device), labels.to(device)
         optimizer.zero_grad()
-
-        # forward + backward + optimize
         outputs = net(inputs)
         loss = loss_func(outputs, labels)
         loss.backward()
         optimizer.step()
 
         running_loss += loss.item()
-    print(f'Epoch {epoch + 1}, Loss: {running_loss / len(train_loader)}')
+
+    net.eval()  # Set the network to evaluation mode
+    val_loss = 0.0
+    with torch.no_grad():
+        for inputs, labels in val_loader:
+            inputs, labels = inputs.to(device), labels.to(device)
+            outputs = net(inputs)
+            loss = loss_func(outputs, labels)
+            val_loss += loss.item()
+
+    print(f'Epoch {epoch + 1}, Training Loss: {running_loss / len(train_loader)}, Validation Loss: {val_loss / len(val_loader)}')
 
 print('Finished Training')
 
 
-# test net
+# Test net
 net.eval()
 y_pred = []
 with torch.no_grad():
     for inputs, labels in test_loader:
+        inputs, labels = inputs.to(device), labels.to(device)
         outputs = net(inputs)
         _, predicted = torch.max(outputs.data, 1)
-        y_pred.extend(predicted.numpy())
-        
+        y_pred.extend(predicted.cpu().numpy())
         
 # accuracy
 accuracy = accuracy_score(y_test.numpy(), np.array(y_pred))
 print(f'Accuracy: {accuracy * 100:.2f}%')
-
 
 
 
